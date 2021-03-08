d880cfc6f1aa5190f0e5248bae5bfc8e96bc15d3
Michael.Figueroa <michael.figueroa@figstudios.io>
2021-03-07 18:22:50 -0800

    staticfiles and pipenv implemented

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7t2q6bopz6fl@hqfq2da9ypudzigja*b3p57n!iia6m&%l!1-w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-	'USER': 'postgres',
-	'PASSWORD': 'postgres',
-	'HOST': 'db',
-	'PORT': 5432,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT', default='5432'),
     }
 }
 
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- config/app/on-container-start.sh
+++ config/app/on-container-start.sh
@@ -1,5 +1,19 @@
-python manage.py makemigrations
+#!/bin/sh
 
-python manage.py migrate
+# wait for postgres container to start
 
-gunicorn --reload myproject.wsgi:application -b 0.0.0.0:8000
+echo "waiting for database to start" && sleep 15
+
+pipenv run python manage.py collectstatic --no-input
+
+# run migrations
+pipenv run python manage.py migrate
+
+#pipenv run python manage.py loaddata fixtures/*.json
+
+# coverage implementation
+pipenv run coverage run -m pytest
+pipenv run coverage html
+# start dev server
+pipenv run gunicorn --reload myproject.wsgi:application -b 0.0.0.0:8000
+
